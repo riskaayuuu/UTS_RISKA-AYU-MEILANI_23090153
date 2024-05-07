189b49c56e914bfb2eefca621d8a14cd5a4a3494
riskaayuuu <145608136+riskaayuuu@users.noreply.github.com>
2024-05-07 17:08:41 +0700

    Add files via upload

--- Soal_1.py
+++ Soal_1.py
@@ -10,23 +10,13 @@ def modulus(angka):
     else:
         return 'Ganjil'
 
-def operasi_bilangan():
-   a = int(input('Masukkan bilangan pertama: '))
-   b = int(input('Masukkan bilangan kedua: ')) 
+bilangan_pertama = int(input('Masukkan bilangan pertama:'))
+bilangan_kedua = int(input('Masukkan bilangan kedua:'))
 
-hasil_penjumlahan = penjumlahan(a, b)
-print(f"Hasil penjumlahan {a} + {b} = {hasil_penjumlahan}")  
-hasil_pengurangan = pengurangan(a, b)
-print(f"Hasil pengurangan {a} - {b} = {hasil_pengurangan}")        
-hasil_modulus_a = modulus(a)
-print(f"Modulus {a} adalah {hasil_modulus_a}")
-hasil_modulus_b = modulus(b)
-print(f"Modulus {b} adalah {hasil_modulus_b}")
-
-
-(operasi_bilangan)       
-          
-
+print('Hasil penjumlahan dari bilangan tersebut adalah ', penjumlahan(bilangan_pertama,bilangan_kedua))
+print('Hasil pengurangan dari bilangan tersebut adalah ', pengurangan(bilangan_pertama,bilangan_kedua))
+print('Hasil modulus dari bilangan tersebut adalah ', modulus(bilangan_pertama))
+print('Hasil modulus dari bilangan tersebut adalah ', modulus(bilangan_kedua))
 
 
 
